0a5773506b6f027c5afb3ff6e58bcb5a6c520596
GabrielAdrianoRodrigues <gabrieladriano@furb.br>
2022-09-19 20:04:17 -0300

    finalizado acerte o palpite

--- acerteOPalpite/Main.lua
+++ acerteOPalpite/Main.lua
@@ -7,7 +7,21 @@ manual()
 
 local palpite = capturaPalpite()
 
-while interacao(numeroGerado, compara(numeroGerado, palpite)) do
-    palpite = capturaPalpite()
-end
+function jogo() 
+    local continuar = true
+    local resposta;
+    while true do
+        while interacao(numeroGerado, compara(numeroGerado, palpite)) do
+            palpite = capturaPalpite()
+        end
+        print("Deseja continuar jogando?")
+        resposta = io.read()
+        if string.upper(resposta) == "S" then
 
+        elseif string.upper(resposta) == "N" then
+            return
+        else 
+            print("Resposta inválida responsa apenas com s ou n")
+        end    
+    end
+end
